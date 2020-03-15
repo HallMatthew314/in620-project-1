5264f1e1d0ad159bf2d13313992fd1db8c8d9cc5
Matthew Hall <hallmg1@student.op.ac.nz>
2020-03-16 09:43:27 +1300

    Responding to interrupts, but not in the way it should

--- timing_game/timing_game.ino
+++ timing_game/timing_game.ino
@@ -1,16 +1,24 @@
 const int LED_START = 6;
 const int LED_MIDDLE = 9;
 const int LED_END = 12;
+const int BUTTON = 2;
 
 int pauseLength;
 int currentLED;
 bool movingUp;
+bool buttonPressed;
 
 void setup() {
-    // Configure LED pins
+
+    // For testing interrupts
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    // Configure pins
     for(int i = LED_START; i <= LED_END; ++i){
         pinMode(i, OUTPUT);
     }
+
+    pinMode(BUTTON, INPUT_PULLUP);
 
     // Start on the first LED
     // This is actually the second LED, but the
@@ -19,14 +27,31 @@ void setup() {
     movingUp = false;
 
     pauseLength = 500;
+    buttonPressed = false;
+
+    // Interrupt when the button is pressed.
+    attachInterrupt(digitalPinToInterrupt(BUTTON), buttonPressActions, LOW);
 }
 
 void loop() {
-    nextLED();
-    delay(pauseLength);
+    if(buttonPressed){
+        noInterrupts();
+
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(2000);
+        digitalWrite(LED_BUILTIN, LOW);
+        buttonPressed = false;
+
+        interrupts();
+    }
+    else{
+        nextLED();
+        delay(pauseLength);
+    }
 }
 
 void nextLED(){
+    noInterrupts();
 
     // Turn off the the current LED
     digitalWrite(currentLED, LOW);
@@ -41,4 +66,10 @@ void nextLED(){
 
     // Turn on the next LED
     digitalWrite(currentLED, HIGH);
+
+    interrupts();
+}
+
+void buttonPressActions(){
+    buttonPressed = true;
 }
